674455987a7fe743f62cefcb7da5d2ceefa90d98
Naimul02 <naimulislum39@gmail.com>
2024-03-25 20:11:57 +0600

    added navbar section

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>{/* Banner Section */}</div>;
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import MainLayout from "./Layout/MainLayout.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import ListedBooks from "./Pages/ListedBooks/ListedBooks.jsx";
+import PagesToRead from "./Pages/PagesToRead/PagesToRead.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/listedbooks",
+        element: <ListedBooks></ListedBooks>,
+      },
+      {
+        path: "/pagestoread",
+        element: <PagesToRead></PagesToRead>,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
