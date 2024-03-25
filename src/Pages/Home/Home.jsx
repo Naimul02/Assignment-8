7ffd7f5ee13e024abcdfa87274ae84e47c6a27af
Naimul02 <naimulislum39@gmail.com>
2024-03-25 21:00:00 +0600

    added banner section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div>{/* Banner Section */}</div>;
+  return (
+    <div>
+      {/* Banner Section */}
+      <div className="rounded-xl bg-base-200 flex justify-around items-center py-10 mt-4">
+        <div>
+          <h1 className="text-5xl font-bold">
+            Books to freshen <br /> up your bookshelf
+          </h1>
+          <Link to="/listedbooks">
+            {" "}
+            <button className="btn bg-green-400 px-6 mt-4 font-semibold hover:bg-blue-400 hover:text-white">
+              View The List
+            </button>
+          </Link>
+        </div>
+        <div>
+          <img src="https://i.ibb.co/zf9QR07/pngwing-1.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,14 @@
+
+
+const Books = () => {
+  return (
+    <div>
+          <h1 className="text-4xl font-bold text-center">Books</h1>
+          <div>
+
+          </div>
+    </div>
+  );
+};
+
+export default Books;
